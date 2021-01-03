a7862517bdb86f027c56d7a20ecfe545d94d5dab
blsandoval1 <blsandoval1@espe.edu.ec>
2021-01-03 18:06:41 -0500

    Revision DAO

--- src/main/java/com/asistencias/backend/controllers/dao/IEmpleado.java
+++ src/main/java/com/asistencias/backend/controllers/dao/IEmpleado.java
@@ -4,6 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.asistencias.backend.models.entities.Empleado;
 
+//DAO sin error.
+
 public interface IEmpleado extends CrudRepository<Empleado,Long> {
 
 }
